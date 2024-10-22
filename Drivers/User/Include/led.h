18c3588a5997a41632c400b21c66c8ec62b55ccf
ylongwang2782 <ylongwangi@qq.com>
2024-10-22 17:21:33 +0800

    Add: 新增led的CPP类文件

---
+++ Drivers/User/Include/led.h
@@ -0,0 +1,33 @@
+/**
+ * @file led.h
+ * @author wang.yunlong (wang.yunlong9@byd.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-10-22
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "gd32f4xx.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+class LED
+{
+   private:
+    uint32_t port;
+    uint32_t pin;
+
+   public:
+    LED(rcu_periph_enum rcc, uint32_t port, uint32_t pin);
+    ~LED();
+    void on(void);
+    void off(void);
+    void toggle(void);
+};
