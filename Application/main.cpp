b9e72bf0e2424f5fd30742c094d6e396c03aa400
ylongwang2782 <ylongwangi@qq.com>
2024-10-29 14:43:30 +0800

    Style: 简化一些代码

--- Application/main.cpp
+++ Application/main.cpp
@@ -1,51 +1,12 @@
-
-#include <array>
-#include <cstdint>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#include "systick.h"
-
-#ifdef __cplusplus
-}
-#endif
-#include "chronolink.h"
-#include "com.h"
-#include "json_interface.h"
-#include "led.h"
-#include "timer.h"
+#include "main.h"
 
 LED led0(RCU_GPIOC, GPIOC, GPIO_PIN_6);
+void ledTask() { led0.toggle(); }
 
 extern SerialConfig usart1_config;
 Serial com1(usart1_config);
 
-void ledTask() { led0.toggle(); }
-
-class UIDReader {
-   public:
-    UIDReader(uint32_t address) {
-        uidAddress = reinterpret_cast<uint32_t *>(address);
-        readUID();
-    }
-
-    const std::array<uint8_t, 4> &getUID() const { return uidArray; }
-
-   private:
-    uint32_t *uidAddress;
-    std::array<uint8_t, 4> uidArray;
-
-    void readUID() {
-        uint32_t uid = *uidAddress;
-        for (int i = 0; i < 4; ++i) {
-            uidArray[i] = static_cast<uint8_t>((uid >> (24 - i * 8)) & 0xFF);
-        }
-    }
-};
+std::array<uint8_t, 4> UIDReader::UID;
 
 int main(void) {
     systick_config();
@@ -55,8 +16,8 @@ int main(void) {
     Timer timer(50, ledTask);
 
     UIDReader uidReader(0x1FFF7A10);
-    const auto &uid = uidReader.getUID();
-    printf("UID: %X%X%X%X\n", uid[0], uid[1], uid[2], uid[3]);
+    printf("UID: %X%X%X%X\n", UIDReader::UID[0], UIDReader::UID[1],
+           UIDReader::UID[2], UIDReader::UID[3]);
 
     while (1) {
         if (usart1_config.rx_count > 0) {
