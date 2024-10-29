a0ba956879e504d60c02e48e443718fd7c5619d7
ylongwang2782 <ylongwangi@qq.com>
2024-10-29 14:42:45 +0800

    Add: 新增main.h文件

---
+++ Application/main.h
@@ -0,0 +1,31 @@
+#include <array>
+#include <cstdlib>
+#include <cstring>
+extern "C" {
+#include "systick.h"
+}
+#include "chronolink.h"
+#include "com.h"
+#include "json_interface.h"
+#include "led.h"
+#include "timer.h"
+
+class UIDReader {
+   public:
+    UIDReader(uint32_t address) {
+        uidAddress = reinterpret_cast<uint32_t *>(address);
+        readUID();
+    }
+
+    static std::array<uint8_t, 4> UID;
+
+   private:
+    uint32_t *uidAddress;
+
+    void readUID() {
+        uint32_t uid = *uidAddress;
+        for (int i = 0; i < 4; ++i) {
+            UID[i] = static_cast<uint8_t>((uid >> (24 - i * 8)) & 0xFF);
+        }
+    }
+};
