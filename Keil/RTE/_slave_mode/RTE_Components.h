69fd8213a21435dc741608b5e8c097d2f3b5518f
ylongwang2782 <ylongwangi@qq.com>
2024-11-22 16:27:21 +0800

    Refactor：keil工程更新

--- Keil/RTE/_slave_mode/RTE_Components.h
+++ Keil/RTE/_slave_mode/RTE_Components.h
@@ -15,6 +15,15 @@
  */
 #define CMSIS_device_header "gd32f4xx.h"
 
+/* Keil::Compiler&ARM Compiler:I/O:STDERR&Breakpoint@1.2.0 */
+#define RTE_Compiler_IO_STDERR          /* Compiler I/O: STDERR */
+#define RTE_Compiler_IO_STDERR_BKPT     /* Compiler I/O: STDERR Breakpoint */
+/* Keil::Compiler&ARM Compiler:I/O:STDIN&Breakpoint@1.2.0 */
+#define RTE_Compiler_IO_STDIN           /* Compiler I/O: STDIN */
+#define RTE_Compiler_IO_STDIN_BKPT      /* Compiler I/O: STDIN Breakpoint */
+/* Keil::Compiler&ARM Compiler:I/O:STDOUT&Breakpoint@1.2.0 */
+#define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
+#define RTE_Compiler_IO_STDOUT_BKPT     /* Compiler I/O: STDOUT Breakpoint */
 
 
 #endif /* RTE_COMPONENTS_H */
