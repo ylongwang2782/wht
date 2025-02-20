baacdae42153a08a65df979f6951ff7b9f65eee8
rainlong <ylongwangi@qq.com>
2025-02-20 16:19:17 +0800

    [Refactor] (Harness): 将函数实现从cpp文件移至头文件
    将 `Harness` 类的成员函数实现从 `harness.cpp` 文件移至 `harness.h` 头文件中。
    此更改简化了代码结构，减少了文件间的依赖关系，并提高了代码的可读性和维护性。

--- Source/Core/slave/harness.h
+++ Source/Core/slave/harness.h
@@ -46,12 +46,41 @@ class Harness {
                                                          {GPIOE, 1 << 8},
                                                          {GPIOE, 1 << 9}}};
     std::vector<uint8_t> result;
-    uint8_t collect_pin_states();
-    void init();
+    
+    uint8_t collect_pin_states() {
+        for (matrix.col_index = 0; matrix.col_index < matrix.col;
+             matrix.col_index++) {
+            const auto& gpio_pin = pin_map[matrix.col_index];
+            result.push_back(gpio_input_bit_get(gpio_pin.port, gpio_pin.pin));
+        }
+        return 0;
+    }
+
+    void init() {
+        // enable clock for corresponding GPIO port
+        rcu_periph_clock_enable(RCU_GPIOE);
+        // reset all pins
+        for (size_t i = 0; i < pin_map.size(); i++) {
+            slave_pin_set(i);
+        }
+    }
+
     void start();
-    bool data_get(uint8_t *data);
-    void master_pin_set(uint8_t pin_num);
-    void slave_pin_set(uint8_t pin_num);
+
+    bool data_get(uint8_t* data) { return true; }
+
+    void master_pin_set(uint8_t pin_num) {
+        const GpioPin& selected_pin = pin_map[pin_num];
+        gpio_mode_set(selected_pin.port, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE,
+                      selected_pin.pin);
+        gpio_output_options_set(selected_pin.port, GPIO_OTYPE_PP,
+                                GPIO_OSPEED_50MHZ, selected_pin.pin);
+    }
+    void slave_pin_set(uint8_t pin_num) {
+        const GpioPin& selected_pin = pin_map[pin_num];
+        gpio_mode_set(selected_pin.port, GPIO_MODE_INPUT, GPIO_PUPD_NONE,
+                      selected_pin.pin);
+    }
 
     Matrix matrix;
 
