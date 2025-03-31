c9cb9417f139269d7c491aed6bcdb15f3d243110
PainterCGJ <571928107@qq.com>
2025-03-31 16:19:36 +0800

    refactor(master_def): reorganize command structures and enhance configuration parameters

--- Source/Core/master/master_def.hpp
+++ Source/Core/master/master_def.hpp
@@ -3,6 +3,7 @@
 #include <cstddef>
 #include <cstdint>
 #include <iterator>
+#include <vector>
 
 #include "EventCPP.h"
 #include "Lock.h"
@@ -11,68 +12,8 @@
 #include "SemaphoreCPP.h"
 #include "master_cfg.hpp"
 #include "portable.h"
-enum CmdType : uint8_t {
-    DEV_CONF = 0,
-    DEV_MODE,
-    DEV_RESET,
-    DEV_CTRL,
-    DEV_QUERY,
-};
 
-enum CtrlType : uint8_t {
-    DEV_DISABLE = 0,
-    DEV_ENABLE,
-};
-
-enum SysMode : uint8_t {
-    CONDUCTION_TEST = 0,
-    IMPEDANCE_TEST,
-    CLIP_TEST
-
-};
-
-enum LockSta : uint8_t {
-    UNLOCKED = 0,
-    LOCKED,
-};
-
-struct CfgCmd {
-    uint8_t id[4];
-    uint16_t cond;
-    uint16_t Z;
-    uint16_t clip;
-    uint16_t totalHarnessNum;
-    uint16_t startHarnessNum;
-};
-
-struct ModeCmd {
-    SysMode mode;
-};
-
-struct ResetCmd {
-    uint8_t id[4];
-    uint16_t clip;
-    LockSta lock;
-};
-
-struct CtrlCmd {
-    CtrlType ctrl;
-};
-
-struct QueryCmd {
-    uint8_t id[4];
-    uint8_t clip;
-};
-
-struct DataForward {
-    CmdType type;
-    CfgCmd cfg_cmd;
-    ModeCmd mode_cmd;
-    ResetCmd rst_cmd;
-    CtrlCmd ctrl_cmd;
-    QueryCmd query_cmd;
-};
-
+// 迭代器-------------------------------------------------
 class Uint8ArrayIterator {
    public:
     using iterator_category = std::output_iterator_tag;
@@ -104,6 +45,7 @@ class Uint8ArrayIterator {
     uint8_t* ptr_;
 };
 
+// 共享内存-------------------------------------------------
 class ShareMem {
    public:
     ShareMem() : __mutex("mem"), __lock(__mutex) {}
@@ -136,6 +78,77 @@ class ShareMem {
     uint8_t* shared_mem;
 };
 
+// 配置指令-------------------------------------------------
+struct CfgCmd {
+    uint8_t id[4];
+    uint16_t cond;
+    uint16_t Z;
+    
+    bool clip_exist;
+    uint8_t clip_mode;
+    uint16_t clip_pin;
+
+    uint16_t totalHarnessNum;
+    uint16_t startHarnessNum;
+    bool is_last_dev;
+};
+
+// 模式指令-------------------------------------------------
+enum SysMode : uint8_t {
+    CONDUCTION_TEST = 0,
+    IMPEDANCE_TEST,
+    CLIP_TEST
+
+};
+struct ModeCmd {
+    SysMode mode;
+};
+
+// 复位指令-------------------------------------------------
+enum LockSta : uint8_t {
+    UNLOCKED = 0,
+    LOCKED,
+};
+struct ResetCmd {
+    uint8_t id[4];
+    uint16_t clip;
+    LockSta lock;
+};
+
+// 控制指令-------------------------------------------------
+enum CtrlType : uint8_t {
+    DEV_DISABLE = 0,
+    DEV_ENABLE,
+};
+struct CtrlCmd {
+    CtrlType ctrl;
+};
+
+// 查询指令-------------------------------------------------
+struct QueryCmd {
+    uint8_t id[4];
+    uint8_t clip;
+};
+
+// 转发数据-------------------------------------------------
+enum CmdType : uint8_t {
+    DEV_CONF = 0,
+    DEV_MODE,
+    DEV_RESET,
+    DEV_CTRL,
+    DEV_QUERY,
+};
+struct DataForward {
+    CmdType type;
+    CfgCmd cfg_cmd;
+    ModeCmd mode_cmd;
+    ResetCmd rst_cmd;
+    CtrlCmd ctrl_cmd;
+    QueryCmd query_cmd;
+};
+
+// 上位机数据传输任务 <-> json解析任务
+// 消息结构---------------------------------------
 class PCdataTransferMsg {
    public:
     PCdataTransferMsg()
@@ -151,6 +164,8 @@ class PCdataTransferMsg {
     ShareMem tx_share_mem;
 };
 
+// json解析任务 <-> 从机管理任务
+// 消息结构---------------------------------------------
 #define FORWARD_SUCCESS_EVENT (EventBits_t)((EventBits_t)1 << 0)
 #define CONFIG_SUCCESS_EVENT  (EventBits_t)((EventBits_t)1 << 1)
 #define MODE_SUCCESS_EVENT    (EventBits_t)((EventBits_t)1 << 2)
@@ -161,5 +176,26 @@ class PCmanagerMsg {
    public:
     Queue<DataForward, PCmanagerMsg_FORWARD_QUEUE_SIZE> data_forward_queue;
     EventGroup event;
+};
+
+// 从机数据传输任务 <-> 从机管理任务
+// 消息结构-----------------------------------------------------
+class ManagerDataTransferMsg {
+   public:
+    ManagerDataTransferMsg()
+        : rx_done_sem("rx_done_sem"),
+          tx_request_sem("tx_request_sem"),
+          tx_done_sem("tx_done_sem"),
+
+          tx_data_queue("manager_tx_data_queue"),
+          rx_data_queue("manager_rx_data_queue") {}
+
+   public:
+    BinarySemaphore rx_done_sem;
+    BinarySemaphore tx_request_sem;
+    BinarySemaphore tx_done_sem;
+
+    Queue<uint8_t, ManagerDataTransferMsg_RXDATA_QUEUE_SIZE> tx_data_queue;
+    Queue<uint8_t, ManagerDataTransferMsg_RXDATA_QUEUE_SIZE> rx_data_queue;
 };
 #endif
