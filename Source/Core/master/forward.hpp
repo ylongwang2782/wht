ab8196ca3b26755b5cf8fa1143d6a550fd5fa100
PainterCGJ <571928107@qq.com>
2025-04-17 08:51:40 +0800

    更改：将数据转发和json解析独立成不同的文件

---
+++ Source/Core/master/forward.hpp
@@ -0,0 +1,39 @@
+#ifndef __FORWARD_HPP
+#define __FORWARD_HPP
+#include <algorithm>
+
+#include "bsp_log.hpp"
+#include "master_cfg.hpp"
+#include "master_def.hpp"
+extern Logger Log;
+class DataForwardBase {
+   public:
+    DataForwardBase(PCmanagerMsg& msg) : pc_manager_msg(msg) {
+        std::fill(reinterpret_cast<char*>(&data_forward),
+                  reinterpret_cast<char*>(&data_forward) + sizeof(data_forward),
+                  0);
+    };
+    ~DataForwardBase() {};
+
+    DataForward data_forward;
+    PCmanagerMsg& pc_manager_msg;
+
+    bool forward() {
+        if (pc_manager_msg.data_forward_queue.add(
+                data_forward, PCinterface_FORWARD_QUEUE_TIMEOUT)) {
+        } else {
+            Log.e("[Forward]: pc_manager_msg.data_forward_queue.add failed");
+            return false;
+        }
+
+        if (pc_manager_msg.event.wait(FORWARD_SUCCESS_EVENT, true, true,
+                                      PCinterface_FORWARD_TIMEOUT)) {
+        } else {
+            Log.e("[Forward]: pc_manager_msg.event.take failed");
+            return false;
+        }
+        return true;
+    }
+};
+
+#endif
