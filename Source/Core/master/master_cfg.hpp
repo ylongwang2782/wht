569e20b1ee09767fff3ddc4b07e2b0ad0b494655
PainterCGJ <571928107@qq.com>
2025-04-18 17:12:57 +0800

    更改：修正同步定时器冗余时间为0，添加slave_num成员变量并实现控制处理逻辑，优化从机管理任务的控制流程

--- Source/Core/master/master_cfg.hpp
+++ Source/Core/master/master_cfg.hpp
@@ -6,7 +6,7 @@
 
 #define CONDUCTION_TEST_INTERVAL           5      // 导通检测时间间隔
 #define CLIP_TEST_INTERVAL                 20     // 卡钉检测时间间隔
-#define SYNC_TIMER_PERIOD_REDUNDANCY_TICKS 500    // 同步定时器冗余时间
+#define SYNC_TIMER_PERIOD_REDUNDANCY_TICKS 0    // 同步定时器冗余时间
 #define PC_TX_SHARE_MEM_ACCESS_TIMEOUT \
     1000    // 获取上位机数据传输共享内存写权限超时
 #define PC_TX_TIMEOUT 1000    // 向上位机发送数据超时
