0fff48092c5c6007e9277802923db9756513ae5a
rainlong <ylongwangi@qq.com>
2025-04-24 16:20:48 +0800

    [Fix] (led) 修复了bsp led中on off中的逻辑错误

--- Source/BSP/inc/bsp_led.hpp
+++ Source/BSP/inc/bsp_led.hpp
@@ -4,8 +4,8 @@ class LED {
    public:
     LED(GPIO::Port port, GPIO::Pin pin) : gpio(port, pin, GPIO::Mode::OUTPUT) {}
 
-    void on() { gpio.bit_set(); }
-    void off() { gpio.bit_reset(); }
+    void on() { gpio.bit_reset(); }
+    void off() { gpio.bit_set(); }
     void toggle() { gpio.toggle(); }
 
    private:
